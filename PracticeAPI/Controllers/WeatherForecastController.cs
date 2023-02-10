ce468e5bed9513253155aed7be610a53c02be7c0
DanilkaRavdinka <124984811+DanilkaRavdinka@users.noreply.github.com>
2023-02-10 11:01:40 +0300

    Add files via upload

---
+++ PracticeAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static List<string> Summaries = new()
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<string> Get()
+        {
+            return Summaries;
+        }
+        [HttpGet("{index}")]
+        public string GetIndexWord(int index)
+        {
+            if (index < 0 || index >= Summaries.Count)
+            {
+                return "Такой индекс неверный";
+            }
+
+            return Summaries[index];
+        }
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            if(name == null)
+            {
+                return BadRequest("Пустое поле");
+            }
+
+            Summaries.Add(name);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult Update(int index, string name)
+        {
+            if (index < 0 || index >= Summaries.Count)
+            {
+                return BadRequest("Такой индекс неверный");
+            }
+
+            if (name == null)
+            {
+                return BadRequest("Пустое поле");
+            }
+
+            Summaries[index] = name;
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult Delete(int index)
+        {
+            if (index < 0 || index >= Summaries.Count)
+            {
+                return BadRequest("Такой индекс неверный");
+            }
+
+            Summaries.RemoveAt(index);
+            return Ok();
+        }
+    }
+}
